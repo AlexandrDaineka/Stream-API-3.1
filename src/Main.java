e638ce6e976e40ebb8270c7633ee7c68a9fad73c
Daineka A.U <zanoza193@mail.ru>
2023-05-09 17:39:03 +0300

    2 commit

--- src/Main.java
+++ src/Main.java
@@ -3,78 +3,49 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    private static ArrayList filesList = new ArrayList<>();
+    public static void main(String[] args) {
+        createDirectories();
+        createFiles();
+        writeLog();
+    }
 
-        File gamesDir = new File("C:/Games/Games");
-        gamesDir.mkdir();
+    private static void createDirectories() {
+        createDirectory("C:/Games/Games");
+        createSubDirectories("C:/Games/Games/src", "C:/Games/Games/res", "C:/Games/Games/savegames", "C:/Games/Games/temp");
+        createSubDirectories("C:/Games/Games/src/main", "C:/Games/Games/src/test");
+        createSubDirectories("C:/Games/Games/res/drawables", "C:/Games/Games/res/vectors", "C:/Games/Games/res/icons");
+    }
 
-        File srcDir = new File(gamesDir, "src");
-        srcDir.mkdir();
+    private static void createFiles() {
+        createFile("C:/Games/Games/src/main/Main.java");
+        createFile("C:/Games/Games/src/main/Utils.java");
+        createFile("C:/Games/Games/temp/temp.txt");
+        filesList.add("C:/Games/Games/res/drawables");
+        filesList.add("C:/Games/Games/res/vectors");
+        filesList.add("C:/Games/Games/res/icons");
+        filesList.add("C:/Games/Games/savegames");
+        filesList.add("C:/Games/Games/temp");
+    }
 
-        File mainDir = new File(srcDir, "main");
-        mainDir.mkdir();
-
-        File testDir = new File(srcDir, "test");
-        testDir.mkdir();
-
-        File mainJava = new File(mainDir, "Main.java");
-        try {
-            mainJava.createNewFile();
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-
-        File utilsJava = new File(mainDir, "Utils.java");
-        try {
-            utilsJava.createNewFile();
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-
-        File resDir = new File(gamesDir, "res");
-        resDir.mkdir();
-
-        File drawablesDir = new File(resDir, "drawables");
-        drawablesDir.mkdir();
-
-        File vectorsDir = new File(resDir, "vectors");
-        vectorsDir.mkdir();
-
-        File iconsDir = new File(resDir, "icons");
-        iconsDir.mkdir();
-
-        File savegamesDir = new File(gamesDir, "savegames");
-        savegamesDir.mkdir();
-
-        File tempDir = new File(gamesDir, "temp");
-        tempDir.mkdir();
-
-        File tempFile = new File(tempDir, "temp.txt");
-        try {
-            tempFile.createNewFile();
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-
+    private static void writeLog() {
+        File tempFile = new File("C:/Games/Games/temp/temp.txt");
         StringBuilder log = new StringBuilder();
         DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
         Date date = new Date();
-
         String currentUser = System.getProperty("user.name");
 
         log.append(dateFormat.format(date)).append(": ");
         log.append(currentUser).append(" установил(а) следующие файлы:\n");
-        log.append(mainJava.getPath()).append("\n");
-        log.append(utilsJava.getPath()).append("\n");
-        log.append(drawablesDir.getPath()).append("\n");
-        log.append(vectorsDir.getPath()).append("\n");
-        log.append(iconsDir.getPath()).append("\n");
-        log.append(savegamesDir.getPath()).append("\n");
-        log.append(tempDir.getPath()).append("\n");
-        log.append(tempFile.getPath()).append("\n");
+
+        for (Object file : filesList) {
+            log.append(file).append("\n");
+        }
+
         log.append("Совокупное время установки всех файлов: ").append(System.currentTimeMillis()).append(" милисекунд\n");
 
         try (FileWriter writer = new FileWriter(tempFile)) {
@@ -82,8 +53,42 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
+    private static void createDirectory(String path) {
+        File newDir = new File(path);
+        boolean isCreated = newDir.mkdir();
+        if (isCreated) {
+            String successMessage = "Папка " + path + " успешно создана";
+            System.out.println(successMessage);
+            filesList.add(path);
+        } else {
+            String errorMessage = "Не удалось создать папку " + path;
+            System.out.println(errorMessage);
+        }
+    }
 
+    private static void createFile(String path) {
+        File newFile = new File(path);
+        boolean isCreated;
+        try {
+            isCreated = newFile.createNewFile();
+        } catch (IOException ex) {
+            isCreated = false;
+        }
+        if (isCreated) {
+            String successMessage = "Файл " + path + " успешно создан";
+            System.out.println(successMessage);
+            filesList.add(path);
+        } else {
+            String errorMessage = "Не удалось создать файл " + path;
+            System.out.println(errorMessage);
+        }
+    }
 
+    private static void createSubDirectories(String... paths) {
+        for (String path : paths) {
+            createDirectory(path);
+        }
     }
 }
